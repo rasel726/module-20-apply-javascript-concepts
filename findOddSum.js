d98a974e2b4486e9c403d700a1bda68ea9008b82
rasel726 <md.rasel2726@gmail.com>
2022-08-08 02:37:56 +0600

    added

--- findOddSum.js
+++ findOddSum.js
@@ -1,13 +1,30 @@
-function getSumOfArray(numbers){
+function getSumOfAnArray(numbers){
     let sum = 0;
     for(let i = 0; i < numbers.length; i++){
         const index = i;
         const element = numbers[index];
         sum = sum + element;
-        console.log(index, element, sum);
+        // console.log(index, element, sum);
     }
     return sum;
 }
 
-const myNumbers = [12, 65, 45, 78, 32, 45, 91]
-getSumOfArray(myNumbers);
+function getOddNumbersOfAnArray(numbers){
+    const oddNumbers =[];
+    for(let i = 0; i < numbers.length; i++){
+        const index = i;
+        const element = numbers[index];
+        if(element % 2 !== 1){
+            console.log(index, element);
+            oddNumbers.push(element);
+        }        
+    }
+    return oddNumbers;
+
+}
+
+const myNumbers = [12, 65, 45, 78, 32, 45, 91];
+const oddNumbers = getOddNumbersOfAnArray(myNumbers);
+console.log(oddNumbers);
+const oddNumberSum = getSumOfAnArray(oddNumbers)
+getSumOfAnArray('odd number sum', oddNumberSum);
